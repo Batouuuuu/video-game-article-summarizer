52c1f4c31bac6ce3121b9e81b21aa268a498b705
Batouuuuu <gillet.baptiste94@gmail.com>
2025-07-02 16:44:40 +0200

    feature : sound + searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import BackgroundVideo from "./components/BackgroundVideo";
+import sonIcon from "./assets/son.png";
+import sound from "./assets/minecraft_cat_sound.mp3";
 
 function App() {
   return (
     <>
-    <BackgroundVideo/>
+      <BackgroundVideo />
+      <div className="app-container">
+        <div className="sound_logo">
+          <audio src={sound} autoPlay />
+          <img src={sonIcon} />
+        </div>
+        <div className="searchbar">
+          <input type="text" placeholder="Recherchez un jeu..."></input>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -1,12 +1,11 @@
-import react from 'react'
-import myVideo from "../assets/minecraft_loop.mp4"
+import myVideo from "../assets/minecraft_loop.mp4";
 
 function BackgroundVideo() {
-    return (
-        <div>
-            <video src= {myVideo} autoPlay muted loop></video>
-        </div>
-    )
+  return (
+    <div>
+      <video src={myVideo} autoPlay muted loop></video>
+    </div>
+  );
 }
 
-export default BackgroundVideo
+export default BackgroundVideo;
